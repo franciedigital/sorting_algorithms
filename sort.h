142ae4e5c5e288092dbadc80555ae718f2d8a588
Franca Mgbogu <franciedigital@gmail.com>
2023-02-22 12:37:53 +0100

    A function that sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth sequence

---
+++ sort.h
@@ -0,0 +1,37 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *);
+void print_list_rev(const listint_t *);
+void bubble_sort(int *, size_t);
+void insertion_sort_list(listint_t **);
+void swap_nodes(listint_t **, listint_t *, listint_t *);
+void selection_sort(int *, size_t);
+void quick_sort(int *, size_t);
+void _qsort(int *, int, int, int);
+void void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **);
+void counting_sort(int *, size_t);
+void merge_sort(int *, size_t);
+void heap_sort(int *, size_t);
+
+#endif
