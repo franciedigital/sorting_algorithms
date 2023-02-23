61b9d6145d2f1fa69a60434e2f62dfdb5005368c
Franca Mgbogu <franciedigital@gmail.com>
2023-02-23 15:48:49 +0100

    fix

--- sort.h
+++ sort.h
@@ -19,7 +19,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
+void print_array(const int *array, size_t);
 void print_list(const listint_t *);
 void print_list_rev(const listint_t *);
 void bubble_sort(int *, size_t);
@@ -28,7 +28,7 @@ void swap_nodes(listint_t **, listint_t *, listint_t *);
 void selection_sort(int *, size_t);
 void quick_sort(int *, size_t);
 void _qsort(int *, int, int, int);
-void void shell_sort(int *array, size_t size);
+void shell_sort(int *, size_t);
 void cocktail_sort_list(listint_t **);
 void counting_sort(int *, size_t);
 void merge_sort(int *, size_t);
